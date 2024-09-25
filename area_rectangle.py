23f8fd703c8adf4e29d9516a5de21d74c467bdc0
Shubham <154921992+ShubhamGawande1011@users.noreply.github.com>
2024-09-25 16:13:50 +0530

    Create area_rectangle.py

---
+++ area_rectangle.py
@@ -0,0 +1,7 @@
+length = float(input("Enter the length of the Rectangle : "))
+
+width = float(input("Enter the width of the Rectangle : "))
+
+area = length * width
+
+print(f"The area of the rectangle is {area}")
