91aac099a85a6828b4e5a2dc43e48f807470def1
Shubham <154921992+ShubhamGawande1011@users.noreply.github.com>
2024-09-25 16:30:43 +0530

    Create perimeter_reactangle.py

---
+++ perimeter_reactangle.py
@@ -0,0 +1,6 @@
+length = float(input("Enter the length of the rectangle :"))
+width  = float(input("Enter the width of the rectangle :"))
+
+perimeter = 2 * (length + width)
+
+print(f"the Perimeter of the reactangle is : {perimeter}")
