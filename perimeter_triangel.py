ec64bb92c8db298ecd002a68894e46c337df163b
Shubham <154921992+ShubhamGawande1011@users.noreply.github.com>
2024-09-25 16:23:17 +0530

    Create perimeter_triangel.py

---
+++ perimeter_triangel.py
@@ -0,0 +1,7 @@
+side1 = float(input("Enter the length of the first side : "))
+side2 = float(input("Enter the length of the second side : "))
+side3 = float(input("Enter the length of the third side : "))
+
+perimeter = side1 + side2 + side3
+
+print(f"The Perimeter of the triangle is : {perimeter}")
